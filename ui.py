c852bb15dd298be1ea613308714f016fbedc19d7
Stanisław Szcześniak <stas.szczesniak@gmail.com>
2015-12-10 18:38:04 +0100

    added main.py and ui.py

---
+++ ui.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python2
+# vim:fileencoding=UTF-8:ts=4:sw=4:sta:et:sts=4:ai
+from __future__ import (unicode_literals, division, absolute_import,
+                        print_function)
+
+__license__   = 'GPL v3'
+__copyright__ = '2011, Kovid Goyal <kovid@kovidgoyal.net>'
+__docformat__ = 'restructuredtext en'
+
+if False:
+    # This is here to keep my python error checker from complaining about
+    # the builtin functions that will be defined by the plugin loading system
+    # You do not need this code in your plugins
+    get_icons = get_resources = None
+
+# The class that all interface action plugins must inherit from
+from calibre.gui2.actions import InterfaceAction
+from calibre_plugins..main import DemoDialog
+
+class InterfacePlugin(InterfaceAction):
+
+    name = 'Interface Plugin Demo'
+
+    action_spec = ('Book Brainz Search', None,
+            'Query BB database', 'Ctrl+Shift+F1')
+
+    def genesis(self):
+        icon = get_icons('images/BB.svg')
+        self.qaction.setIcon(icon)
+        self.qaction.triggered.connect(self.show_dialog)
+
+    def show_dialog(self):
+        # The base plugin object defined in __init__.py
+        base_plugin_object = self.interface_action_base_plugin
+        # Show the config dialog
+        # The config dialog can also be shown from within
+        # Preferences->Plugins, which is why the do_user_config
+        # method is defined on the base plugin class
+        do_user_config = base_plugin_object.do_user_config
+
+        # self.gui is the main calibre GUI. It acts as the gateway to access
+        # all the elements of the calibre user interface, it should also be the
+        # parent of the dialog
+        d = DemoDialog(self.gui, self.qaction.icon(), do_user_config)
+        d.show()
+
+    def apply_settings(self):
+        from calibre_plugins..config import prefs
+        # In an actual non trivial plugin, you would probably need to
+        # do something based on the settings in prefs
+        prefs
+
